4dc8ea9c1d30ad99578540300f588ac145e982fd
Anna Aljanaki <aljanaki@gmail.com>
2024-10-24 15:18:36 +0300

    RTD configurations

---
+++ README.rst
@@ -0,0 +1,5 @@
+Symbolic Corpus of Ukrainian folk music
+=======================================
+
+This documentation contains description for the data in the following repository: 
+https://github.com/aljanaki/Symbolic_Corpus_Of_Ukrainian_Folk_Music/
--- docs/data/conf.py
+++ docs/data/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Symbolic corpus of ukrainian folk music'
+copyright = '2024, Lisniak, Aljanaki, Efremova'
+author = 'aljanaki'
 
 release = '0.1'
 version = '0.1.0'
